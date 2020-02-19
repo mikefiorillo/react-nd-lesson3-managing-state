3bf06d923dc68a8bc238fb726353e30e51b7074b
mikefiorillo <michael.fiorillo@gmail.com>
2020-02-19 13:48:34 -0500

    Split into Game and Score components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,33 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Game from './Game';
+import Score from './Score';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
   state = {
     numQuestions: 0,
     numCorrect: 0
   };
+  handleClick(id, actualAnswer) {
+    if (id === actualAnswer) {
+      this.setState(prev => {
+        return {
+          numCorrect: prev.numCorrect + 1,
+          numQuestions: prev.numQuestions + 1
+        };
+      });
+    } else {
+      this.setState(prev => {
+        return {
+          numQuestions: prev.numQuestions + 1
+        };
+      });
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -15,7 +36,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
-        <Game
+        <Game handleClick={this.handleClick} />
+        <Score
           numQuestions={this.state.numQuestions}
           numCorrect={this.state.numCorrect}
         />
--- src/Game.js
+++ src/Game.js
@@ -1,22 +1,32 @@
 import React from 'react';
 
-const value1 = Math.floor(Math.random() * 100);
-const value2 = Math.floor(Math.random() * 100);
-const value3 = Math.floor(Math.random() * 100);
-const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-
 function Game(props) {
+  const value1 = Math.floor(Math.random() * 100);
+  const value2 = Math.floor(Math.random() * 100);
+  const value3 = Math.floor(Math.random() * 100);
+  const proposedAnswer =
+    Math.floor(Math.random() * 3) + value1 + value2 + value3;
+  const actualAnswer = value1 + value2 + value3 === proposedAnswer;
   return (
     <div className="game">
       <h2>Mental Math</h2>
       <div className="equation">
         <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
       </div>
-      <button>True</button>
-      <button>False</button>
-      <p className="text">
-        Your Score: {props.numCorrect}/{props.numQuestions}
-      </p>
+      <button
+        onClick={() => {
+          props.handleClick(true, actualAnswer);
+        }}
+      >
+        True
+      </button>
+      <button
+        onClick={() => {
+          props.handleClick(false, actualAnswer);
+        }}
+      >
+        False
+      </button>
     </div>
   );
 }
